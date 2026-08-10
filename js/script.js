876256c8ea57478befdce2d96b4ce692007934d0
Manas <shelarmanas1608@gmail.com>
2026-08-10 16:49:14 +0530

    Lab 16 updated code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,92 @@
-let restaurantName = "BiteBox";
-alert("Welcome to " + restaurantName + "!");
+function calculateItemTotal(price, quantity) {
+    return price * quantity;
+}
 
-let customerName = prompt("Welcome to " + restaurantName +"!\n\nWhat is your name ?");
+let cartRows = document.querySelectorAll(
+    "#shopping-cart tbody tr"
+);
 
-alert( "Hello " + customerName + "!\n\nWelcome to BiteBox.");
 
-let exploreMenu = confirm("Would you like to explore our menu?");
+function calculateSubtotal() {
+
+    let subtotal = 0;
+
+    let cartRows = document.querySelectorAll(
+        "#shopping-cart tbody tr"
+    );
+
+    cartRows.forEach(function (row) {
+
+        let priceText = row.children[1].textContent;
+
+        let price = parseFloat(priceText.replace("₹", ""));
+
+        let quantityText = row.querySelector(".quantity-box span").textContent;
+
+        let quantity = parseInt(quantityText);
+
+        let itemTotal = calculateItemTotal(price, quantity);
+
+        subtotal += itemTotal;
+
+    });
+
+    return subtotal;
+}
+
+
+function calculateDeliveryCharge(subtotal) {
+    if (subtotal === 0) {
+        return 0;
+    }
+    return 40;
+}
+
+
+function calculatePackagingCharge(subtotal) {
+    if (subtotal === 0) {
+        return 0;
+    }
+    return 20;
+}
+
+
+function calculateDiscount(subtotal) {
+    if (subtotal > 500) {
+        return subtotal * 0.10;
+    }
+    return 0;
+}
+
+
+function calculateTotal(subtotal, deliveryCharge, packagingCharge, discount) {
+    return subtotal + deliveryCharge + packagingCharge - discount;
+}
+
+
+function updateOrderSummary() {
+
+    let subtotal = calculateSubtotal();
+
+    let deliveryCharge = calculateDeliveryCharge(subtotal);
+
+    let packagingCharge = calculatePackagingCharge(subtotal);
+
+    let discount = calculateDiscount(subtotal);
+
+    let totalAmount = calculateTotal(subtotal,deliveryCharge,packagingCharge,discount);
+
+
+    document.getElementById("subtotal-amount").textContent = "₹" + subtotal.toFixed(2);
+
+    document.getElementById("delivery-amount").textContent = "₹" + deliveryCharge.toFixed(2);
+
+    document.getElementById("packaging-amount").textContent = "₹" + packagingCharge.toFixed(2);
+
+    document.getElementById("discount-amount").textContent = "-₹" + discount.toFixed(2);
+
+    document.getElementById("total-amount").textContent ="₹" + totalAmount.toFixed(2);
+
+}
+
+updateOrderSummary();
