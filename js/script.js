83ea0906e2adcfc11ffabed4e30bd04d1e2ba640
Manas <shelarmanas1608@gmail.com>
2026-08-10 12:41:39 +0530

    Lab 15 updated code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let restaurantName = "BiteBox";
+alert("Welcome to " + restaurantName + "!");
+
+let customerName = prompt("Welcome to " + restaurantName +"!\n\nWhat is your name ?");
+
+alert( "Hello " + customerName + "!\n\nWelcome to BiteBox.");
+
+let exploreMenu = confirm("Would you like to explore our menu?");
