79e86f2926b180b478bfb340588ba1f80765ec6e
Manas <shelarmanas1608@gmail.com>
2026-08-13 13:19:56 +0530

    Lab 18 updated code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function calculateItemTotal(price, quantity) {
     return price * quantity;
 }
@@ -8,27 +9,10 @@ let cartRows = document.querySelectorAll(
 
 
 function calculateSubtotal() {
-
     let subtotal = 0;
-
-    let cartRows = document.querySelectorAll(
-        "#shopping-cart tbody tr"
-    );
-
-    cartRows.forEach(function (row) {
-
-        let priceText = row.children[1].textContent;
-
-        let price = parseFloat(priceText.replace("₹", ""));
-
-        let quantityText = row.querySelector(".quantity-box span").textContent;
-
-        let quantity = parseInt(quantityText);
-
-        let itemTotal = calculateItemTotal(price, quantity);
-
+    cart.forEach(function (item) {
+        let itemTotal = calculateItemTotal(item.price, item.quantity);
         subtotal += itemTotal;
-
     });
 
     return subtotal;
@@ -66,6 +50,13 @@ function calculateTotal(subtotal, deliveryCharge, packagingCharge, discount) {
 
 function updateOrderSummary() {
 
+    let subtotalElement =
+        document.getElementById("subtotal-amount");
+
+    if (!subtotalElement) {
+        return;
+    }
+
     let subtotal = calculateSubtotal();
 
     let deliveryCharge = calculateDeliveryCharge(subtotal);
@@ -74,7 +65,7 @@ function updateOrderSummary() {
 
     let discount = calculateDiscount(subtotal);
 
-    let totalAmount = calculateTotal(subtotal,deliveryCharge,packagingCharge,discount);
+    let totalAmount = calculateTotal(subtotal, deliveryCharge, packagingCharge, discount);
 
 
     document.getElementById("subtotal-amount").textContent = "₹" + subtotal.toFixed(2);
@@ -85,8 +76,382 @@ function updateOrderSummary() {
 
     document.getElementById("discount-amount").textContent = "-₹" + discount.toFixed(2);
 
-    document.getElementById("total-amount").textContent ="₹" + totalAmount.toFixed(2);
+    document.getElementById("total-amount").textContent = "₹" + totalAmount.toFixed(2);
 
 }
 
+let cart = JSON.parse(
+    localStorage.getItem("biteboxCart")
+) || [];
+
+let menuContainer = document.getElementById("menu-items");
+
+
+if (menuContainer) {
+
+    menuContainer.addEventListener(
+        "click",
+        function (event) {
+
+            let button =
+                event.target.closest("button");
+
+
+            if (!button) {
+                return;
+            }
+
+
+            if (
+                button.textContent.includes(
+                    "Add to Cart"
+                )
+            ) {
+
+                let card =
+                    button.closest("article");
+
+
+                addToCart(card);
+
+            }
+
+        }
+    );
+
+}
+
+
+function addToCart(card) {
+    let name = card.querySelector("h3").textContent.trim();
+
+    let priceText = card.querySelector(".price").textContent;
+
+    let price = parseFloat(priceText.replace("₹", ""));
+
+    let image = card.querySelector("img").getAttribute("src");
+
+
+    let cartItem = {
+
+        name: name,
+        price: price,
+        image: image,
+        quantity: 1
+
+    };
+
+    let existingItem =
+        cart.find(function (item) {
+            return item.name === name;
+        });
+
+
+    if (existingItem) {
+        existingItem.quantity++;
+    }
+    else {
+        cart.push(cartItem);
+    }
+
+
+    console.log("Name:", name);
+    console.log("Price:", price);
+    console.log("Image:", image);
+
+    console.log("Cart:", cart);
+
+    localStorage.setItem(
+        "biteboxCart",
+        JSON.stringify(cart)
+    );
+
+    renderCart();
+}
+
+
+let cartBody = document.querySelector("#shopping-cart tbody");
+
+function renderCart() {
+
+    if (!cartBody) {
+        return;
+    }
+
+    cartBody.innerHTML = "";
+
+    if (cart.length === 0) {
+
+        let row = document.createElement("tr");
+
+
+        row.innerHTML = `
+
+        <td colspan="5">
+
+            <div class="empty-cart">
+
+                <h3>
+                    Your Cart is Empty
+                </h3>
+
+                <p>
+                    Add some delicious food
+                    from our Menu!
+                </p>
+
+            </div>
+
+        </td>
+
+    `;
+
+
+        cartBody.appendChild(row);
+
+        return;
+
+    }
+
+
+    cart.forEach(function (item, index) {
+
+        let itemTotal = item.price * item.quantity;
+        let row = document.createElement("tr");
+
+
+        row.innerHTML = `
+
+            <td>
+
+                <div class="cart-item">
+
+                    <img
+                        src="${item.image}"
+                        alt="${item.name}"
+                    >
+
+                    <div>
+
+                        <h3>
+                            ${item.name}
+                        </h3>
+
+                    </div>
+
+                </div>
+
+            </td>
+
+
+            <td>
+                ₹${item.price}
+            </td>
+
+
+            <td>
+
+                <div class="quantity-box">
+
+                    <button
+                        class="decrease"
+                        data-index="${index}">
+                        −
+                    </button>
+
+                    <span>
+                        ${item.quantity}
+                    </span>
+
+                    <button
+                        class="increase"
+                        data-index="${index}">
+                        +
+                    </button>
+
+                </div>
+
+            </td>
+
+
+            <td>
+                ₹${itemTotal.toFixed(2)}
+            </td>
+
+
+            <td>
+
+                 <a href="#" class="remove-item" data-index="${index}">
+
+    <img src="assets/icons/delete.png" alt="Delete" width="25">
+
+    </a>
+
+            </td>
+
+        `;
+
+
+        cartBody.appendChild(row);
+
+    });
+
+}
+
+function increaseQuantity(index) {
+    cart[index].quantity++;
+    localStorage.setItem(
+        "biteboxCart",
+        JSON.stringify(cart)
+    );
+    renderCart();
+    updateOrderSummary();
+}
+
+
+function decreaseQuantity(index) {
+    if (cart[index].quantity > 1) {
+        cart[index].quantity--;
+        localStorage.setItem(
+            "biteboxCart",
+            JSON.stringify(cart)
+        );
+    }
+    renderCart();
+    updateOrderSummary();
+}
+
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("biteboxCart", JSON.stringify(cart));
+
+    renderCart();
+    updateOrderSummary();
+
+}
+
+function clearCart() {
+    cart = [];
+    localStorage.removeItem("biteboxCart");
+    renderCart();
+    updateOrderSummary();
+}
+
+
+let clearCartButton =
+    document.getElementById("clear-cart");
+
+
+if (clearCartButton) {
+
+    clearCartButton.addEventListener(
+        "click",
+        function () {
+
+            clearCart();
+
+        }
+    );
+
+}
+
+
+
+if (cartBody) {
+    cartBody.addEventListener("click", function (event) {
+
+        let button = event.target.closest("button");
+
+        if (button) {
+            let index = parseInt(button.dataset.index);
+            if (button.classList.contains("increase")) {
+                increaseQuantity(index);
+            }
+
+            if (button.classList.contains("decrease")) {
+                decreaseQuantity(index);
+            }
+        }
+
+
+        let link = event.target.closest("a");
+        if (link && link.classList.contains("remove-item")) {
+            event.preventDefault();
+            let index = parseInt(link.dataset.index);
+            removeFromCart(index);
+        }
+
+    });
+}
+
+
+let menuCards = document.querySelectorAll("#menu-items article");
+let searchInput = document.getElementById("menu-search");
+let searchButton = document.getElementById("search-button");
+
+function searchMenu(searchText) {
+    let searchValue = searchText.trim().toLowerCase();
+
+    menuCards.forEach(function (card) {
+        let foodName = card.querySelector("h3").textContent.trim().toLowerCase();
+
+        if (foodName.includes(searchValue)) {
+            card.style.display = "";
+        }
+        else {
+            card.style.display = "none";
+        }
+    });
+
+}
+
+if (searchButton) {
+    searchButton.addEventListener(
+        "click",
+        function () {
+            searchMenu(
+                searchInput.value
+            );
+        }
+    );
+
+}
+
+
+let categoryCards = document.querySelectorAll("#categories article");
+
+function filterMenu(category) {
+
+    menuCards.forEach(function(card) {
+        let cardCategory = card.dataset.category.toLowerCase();
+
+        if (cardCategory === category) {
+            card.style.display = "";
+        }
+        else {
+            card.style.display = "none";
+        }
+    });
+
+}
+
+
+
+categoryCards.forEach(
+    function(card) {
+
+        card.addEventListener("click",function() {
+                let category = card.querySelector("h3")
+                        .textContent
+                        .trim()
+                        .toLowerCase();
+                filterMenu(category);
+            }
+        );
+
+    }
+);
+
+renderCart();
 updateOrderSummary();
